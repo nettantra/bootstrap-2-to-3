1bc0f8dfd491faf7756e275a8c3028880c6ca5f3
devadattas <devadattas@gmail.com>
2014-09-10 01:54:36 +0530

    Updated LESS for testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,18 +4,26 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     // Metadata.
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+    "pkg": grunt.file.readJSON('package.json'),
+    "banner": '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
       '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
       '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
       '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
       ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
     // Task configuration.
-    less: {
-      dist: {
-        options: {
+    "bower-install-simple": {
+      "dev": {
+        "options": {
+          "color": true,
+          "directory": "deps/components"
+        }
+      }
+    },
+    "less": {
+      "dist": {
+        "options": {
         },
-        files: {
+        "files": {
           "dist/bootstrap-2-to-3.css": "less/bootstrap-2-to-3.less"
         }
       }
@@ -23,10 +31,12 @@ module.exports = function(grunt) {
   });
 
   // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-bower-install-simple');
   grunt.loadNpmTasks('grunt-contrib-less');
 
   // Default task.
   grunt.registerTask('build', [
+    'bower-install-simple:dev',
     'less:dist'
   ]);
 
